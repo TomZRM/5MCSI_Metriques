56db030613e8f5f49a6a9972a31c6043f755a4f9
TomZRM <159912631+TomZRM@users.noreply.github.com>
2024-02-13 13:58:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
+from flask import Flask, render_template, jsonify
 from datetime import datetime
 from urllib.request import urlopen
+import json
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #test2
-  
+    return render_template('hello.html')  # Assurez-vous que le fichier hello.html est dans le dossier "templates".
+
+@app.route('/contact/')
+def contact():
+    return "<h2>Ma page de contact</h2>"  # Cette ligne répondra avec un simple message HTML.
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
