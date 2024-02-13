dd54356a71368061306f103059d21053fc5e2ede
TomZRM <159912631+TomZRM@users.noreply.github.com>
2024-02-13 14:01:56 +0100

    Update __init__.py
    tawarano

--- __init__.py
+++ __init__.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template, jsonify
-from datetime import datetime
 from urllib.request import urlopen
 import json
-import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')  # Assurez-vous que le fichier hello.html est dans le dossier "templates".
+    return render_template('hello.html')
 
 @app.route('/contact/')
 def contact():
-    return "<h2>Ma page de contact</h2>"  # Cette ligne répondra avec un simple message HTML.
+    return "<h2>Ma page de contact</h2>"
+
+@app.route('/tawarano/')
+def tawarano():
+    # Remplacez 'xxx' par votre clé API réelle pour OpenWeatherMap si vous en avez une.
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin en °C
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
