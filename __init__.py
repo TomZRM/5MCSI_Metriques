29b40c9f97211dc7abab080691b15ecd87389b8c
TomZRM <159912631+TomZRM@users.noreply.github.com>
2024-02-13 14:06:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,6 +24,11 @@ def tawarano():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Conversion de Kelvin en °C
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+    
+@app.route('/rapport/')
+def rapport():
+    # Cette fonction va rendre la page 'graphique.html' du dossier 'templates'.
+    return render_template('graphique.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
